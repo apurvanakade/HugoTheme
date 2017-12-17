9ef3bf25d04e177ad1df33c2df7f24037b88a0fc
apurvnakade <apurvnakade@gmail.com>
2017-12-16 23:40:56 -0500

    made blog more colorful

--- src/js/siteMap.js
+++ src/js/siteMap.js
@@ -4,9 +4,14 @@ function showSiteMap() {
 }
 
 function hideSiteMap() {
-  document.getElementById("siteMap").style.width = "0%";
+  document.getElementById("siteMap").style.width = "0";
 }
 
 $(window).on('mousemove', function(e) {
   if (e.pageX < 20) showSiteMap();
+});
+
+
+$(document).keyup(function(e) {
+  if (e.keyCode === 27) hideSiteMap();   // esc
 });
