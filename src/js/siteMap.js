d88210bc12fe349e50b396e21d431c06f5567b5f
apurvnakade <apurvnakade@gmail.com>
2017-12-29 17:59:40 -0500

    extra hierarchy of folders

--- src/js/siteMap.js
+++ src/js/siteMap.js
@@ -2,14 +2,16 @@
 var visible = false;
 
 function showSiteMap() {
-  $("#siteMap").css("width","100%");  
+  $("#siteMap").css("width","100%");
   $("#siteMap").css("overflow-y","auto");  
+  $("#marker").css("left","100%");  
   visible = true;
 }
 
 function hideSiteMap() {
   $("#siteMap").css("width","1%");
   $("#siteMap").css("overflow-y","hidden");  
+  $("#marker").css("left","0%");  
   visible = false;
 }
 
