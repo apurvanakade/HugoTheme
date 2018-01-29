3359224823044b42657a8846d03476d1719470e2
apurvnakade <apurvnakade@gmail.com>
2018-01-29 18:09:57 -0500

    added sort utility to archiveTable

--- static/js/sortTable-b1e23afb.js
+++ static/js/sortTable-b1e23afb.js
@@ -1,8 +1,16 @@
+
+function reverseTable(){
+    $('#archiveTable').children('tbody').each(function(elem,index){
+      var arr = $.makeArray($("tr",this).detach());
+      arr.reverse();
+        $(this).append(arr);
+    });
+}
+
 /**
 * @param {integer} n index of the column by which to sort
 * currently using a quadratic sort, could replace by merge sort?
 */
-
 
 function sortTable(n) {
   var table, headers, rows, switching, i, x, y, shouldSwitch, dir;
@@ -13,17 +21,22 @@ function sortTable(n) {
   if(headers[n].classList.contains("sortDesc")) {
     dir = "asc";
     headers[n].classList.replace("sortDesc","sortAsc");
+    reverseTable();
+    switching = false;
   }
   else if(headers[n].classList.contains("sortAsc")) {
     dir = "desc";
     headers[n].classList.replace("sortAsc","sortDesc");
+    reverseTable();
+    switching = false;
   }
   else {
     dir = "asc";
     headers[n].classList.add("sortAsc");
+    switching = true;
   }
 
-  for (i = 0; i < headers.length-1; i++) {
+  for (i = 0; i < headers.length; i++) {
     if(i != n) {
       if(headers[i].classList.contains("sortDesc")) {
         headers[i].classList.remove("sortDesc");
@@ -34,8 +47,7 @@ function sortTable(n) {
     }
   }
 
-  switching = true;
-  while (switching) {
+  while (switching) {    
     switching = false;
     rows = table.getElementsByTagName("TR");
 
@@ -45,12 +57,12 @@ function sortTable(n) {
       y = rows[i + 1].getElementsByTagName("TD")[n];
 
       if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        if (x.innerText.toLowerCase() > y.innerText.toLowerCase()) {
           shouldSwitch= true;
           break;
         }
       } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+        if (x.innerText.toLowerCase() < y.innerText.toLowerCase()) {
           shouldSwitch= true;
           break;
         }
