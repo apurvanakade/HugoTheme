23f34318efc55ed6d84c1742b3e7c53406606328
apurvnakade <apurvnakade@gmail.com>
2018-03-06 13:26:38 -0500

    compactified the archive, removed fb comments

--- src/js/siteMap.js
+++ src/js/siteMap.js
@@ -1,16 +1,62 @@
+/* Seriously need to improve this file */
+
+
+var siteMap;
+//This variable is of use only when called by the parent page.
 function initSiteMap(Permalink) {
-  $("#siteMapFrame").contents().find("#siteMap").find('a').each(function(){
+  var uncollapsed_sections = [];
+  siteMap = $("#siteMapFrame").contents().find("#siteMap");
+  
+  siteMap.find('a').each(function(){
     if ($(this).attr("href") == Permalink) {
       $(this).addClass("you_are_here");
+      if($(this).parent().attr('section'))
+        uncollapsed_sections.push($(this).parent().attr('section'));
+      if($(this).prev().attr('section'))
+        uncollapsed_sections.push($(this).prev().attr('section'));
     }
   });
+  
+  if(uncollapsed_sections.length > 0) {
+    siteMap.find('a.hideShowButton[section]').each(function(){ 
+      var section = this.getAttribute("section");
+      siteMap.find('a.hideShowButton').each(function(){
+        if(this.getAttribute( "section" ) == section) {
+          $(this).html("[+]");
+          $(this).attr("href","javascript:showSections('"+section+"');");
+        }
+      });
+      
+      siteMap.find('div[section]').each(function () {     
+        if(this.getAttribute( "section" ) == section) {
+          $(this).css("display", "none");
+        }
+      });
+    });
+    
+    for (i = 0; i < uncollapsed_sections.length; i++) {
+      var section = uncollapsed_sections[i];
+      siteMap.find('a.hideShowButton').each(function(){
+        if(this.getAttribute( "section" ) == section) {
+          $(this).html("[–]");
+          $(this).attr("href","javascript:hideSections('"+section+"');");
+        }
+      });
+      
+        siteMap.find('div[section]').each(function () {     
+        if(this.getAttribute( "section" ) == section) {
+          $(this).css("display", "");
+        }
+      });
+    }
+  }
 }
 
 function showSiteMap() {
   $("body").css("overflow-y","hidden");  
   $("#siteMapFrame").css("z-index","1");  
-  $("#siteMapFrame").contents().find("#siteMap").css("overflow-y","auto");  
-  $("#siteMapFrame").contents().find("#siteMap").css("opacity","1"); 
+  siteMap.css("overflow-y","auto");  
+  siteMap.css("opacity","1"); 
   $('#marker').html("<p>❮</p>");
   $('#marker').attr("href","javascript:hideSiteMap()");
 }
@@ -18,8 +64,8 @@ function showSiteMap() {
 function hideSiteMap() {
   $("body").css("overflow-y","auto");  
   $("#siteMapFrame").css("z-index","-1");  
-  $("#siteMapFrame").contents().find("#siteMap").css("overflow-y","hidden");  
-  $("#siteMapFrame").contents().find("#siteMap").css("opacity","0"); 
+  siteMap.css("overflow-y","hidden");  
+  siteMap.css("opacity","0"); 
   $('#marker').html("<p>❯</p>");
   $('#marker').attr("href","javascript:showSiteMap()");
 }
